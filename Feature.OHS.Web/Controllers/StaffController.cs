72745fab0c2d2ad6f50d87803a77ec582a986ab0
Vuyiswa <vuyiswa@mgibagroup.com>
2018-10-15 14:56:31 +0200

    creating a nurse

--- Feature.OHS.Web/Controllers/StaffController.cs
+++ Feature.OHS.Web/Controllers/StaffController.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Feature.OHS.Web.Interfaces;
+using Feature.OHS.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Feature.OHS.Web.Controllers
 {
     public class StaffController : Controller
     {
+        private readonly IStaffHandler _staffHandler;
+        public StaffController(IStaffHandler staffHandler)
+        {
+            _staffHandler = staffHandler;
+        }
+
+        public IActionResult Create()
+        {
+            return View(new StaffPayloadViewModel());
+        }
+
         public IActionResult Index()
         {
             return View();
