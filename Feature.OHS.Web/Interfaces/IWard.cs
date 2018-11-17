993f561f2f42bfcdc115e68fcea1494107081be0
onalenna <onalenna@mgibagroup.com>
2018-11-17 03:13:08 +0200

    Added wards code to create and new ward and added validation on the form.

---
+++ Feature.OHS.Web/Interfaces/IWard.cs
@@ -0,0 +1,17 @@
+﻿using Feature.OHS.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Feature.OHS.Web.Interfaces
+{
+    public interface IWard
+    {
+        dynamic CreateWard(WardViewModel ward);
+        dynamic CreateBed(BedViewModel bed);
+        //dynamic CreateRoom()
+        dynamic EditWard(WardViewModel ward);
+        dynamic EditBed(BedViewModel bed);
+    }
+}
