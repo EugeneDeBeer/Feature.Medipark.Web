b31f77bf1b86b4f4a44dd9cc3394e47ea6422001
EugeneDeBeer <42739222+EugeneDeBeer@users.noreply.github.com>
2018-11-28 09:33:22 +0200

    MVP055_ForgotPassword

---
+++ Feature.OHS.Web/Interfaces/IAccountHandler.cs
@@ -0,0 +1,15 @@
+﻿using Feature.OHS.Web.ViewModels;
+using System.Threading.Tasks;
+using Feature.OHS.Web.ViewModels.Response;
+
+namespace Feature.OHS.Web.Interfaces
+{
+    public interface IAccountHandler
+    {
+        Task<PersonViewModel> Register(PersonViewModel person);
+        Task<APIResponse> Login(LoginViewModel model);
+        Task<APIResponse> FindUserByEmail(ForgotPasswordViewModel model);
+        Task<APIResponse> SetUserPasswordResetToken(UpdatePasswordResetTokenModel model);
+        Task<APIResponse> ResetPasswordAsync(ResetPasswordViewModel model);
+    }
+}
