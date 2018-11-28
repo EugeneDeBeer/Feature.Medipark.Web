f98e71c28649b1f099f2b26d34dd01f09770306c
EugeneDeBeer <42739222+EugeneDeBeer@users.noreply.github.com>
2018-11-28 09:35:12 +0200

    MVP055_ForgotPassword

---
+++ Feature.OHS.Web/ViewModels/LoginViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Feature.OHS.Web.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please enter your email address as your {0}")]
+        [DataType(DataType.EmailAddress)]
+        public string UserName { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        [MaxLength(20)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }    
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class UpdatePasswordResetTokenModel
+    {
+        public int UserId { get; set; }    
+        public string ResetToken { get; set; }
+    }
+}
