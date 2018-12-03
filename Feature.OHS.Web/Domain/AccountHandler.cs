ab0ee9f6b95d9b5464777adebb69449762bdcce7
Tsepo Motswaine <tsepo@mgibagroup.com>
2018-12-03 14:08:08 +0200

    Added the base URLS on appsttings.json and updated the AccountHandler to use the Users base URL

--- Feature.OHS.Web/Domain/AccountHandler.cs
+++ Feature.OHS.Web/Domain/AccountHandler.cs
@@ -6,17 +6,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Feature.OHS.Web.Settings;
 using Feature.OHS.Web.ViewModels.Response;
+using Microsoft.Extensions.Options;
 
 namespace Feature.OHS.Web.Domain
 {
     public class AccountHandler : IAccountHandler
     {
         private readonly IAPIIntegration _integration;
+        private readonly IntegrationSettings _integrationSettings;
 
-        public AccountHandler(IAPIIntegration integration)
+        public AccountHandler(IAPIIntegration integration, IOptions<IntegrationSettings> integrationOptions)
         {
             _integration = integration;
+            _integrationSettings = integrationOptions.Value;
         }
 
         public async Task<PersonViewModel> Register(PersonViewModel person)
@@ -37,7 +41,7 @@ namespace Feature.OHS.Web.Domain
             person.UserId = 1;
 
             //var response = _integration.ResponseFromAPIPost("", "v1/Registration/Create", person, "https://localhost:44358/", true);
-            var response = _integration.ResponseFromAPIPost("", "v1/Registration/Create", person, "https://dev-feature-ohs-users-dot-medipark-hospital.appspot.com/", true);
+            var response = _integration.ResponseFromAPIPost("", "v1/Registration/Create", person, _integrationSettings.UsersDevApiUrl, true);
 
             if (response != null)
             {
@@ -54,7 +58,7 @@ namespace Feature.OHS.Web.Domain
         public async Task<APIResponse> Login(LoginViewModel person)
         {
             //var response = _integration.ResponseFromAPIPost("", "v1/Registration", person, "https://localhost:44358/", true);
-            var response = _integration.ResponseFromAPIPost("", "v1/Registration", person, "https://dev-feature-ohs-users-dot-medipark-hospital.appspot.com/", true);
+            var response = _integration.ResponseFromAPIPost("", "v1/Registration", person, _integrationSettings.UsersDevApiUrl, true);
 
             //var responseObject = JsonConvert.DeserializeObject<APIResponse>(response.Message);
             //return responseObject;
@@ -66,7 +70,7 @@ namespace Feature.OHS.Web.Domain
         //public async Task<APIResponse> FindUserByEmail(ForgotPasswordViewModel model)
         public async Task<UserViewModel> FindUserByEmail(ForgotPasswordViewModel model)
         {
-            var response = _integration.ResponseFromAPIPost("", "v1/Registration/FindUserByEmail", model, "https://dev-feature-ohs-users-dot-medipark-hospital.appspot.com/", true);
+            var response = _integration.ResponseFromAPIPost("", "v1/Registration/FindUserByEmail", model, _integrationSettings.UsersDevApiUrl, true);
             //var response = _integration.ResponseFromAPIPost("", "v1/Registration/FindUserByEmail", model, "https://localhost:5001/", true);
 
             if (response != null)
@@ -83,7 +87,7 @@ namespace Feature.OHS.Web.Domain
 
         public async Task<APIResponse> SetUserPasswordResetToken(UpdatePasswordResetTokenModel model)
         {
-            var response = _integration.ResponseFromAPIPost("", "v1/Registration/SetUserPasswordResetToken", new { userId = model.UserId, resetToken = model.ResetToken }, "https://dev-feature-ohs-users-dot-medipark-hospital.appspot.com/", true);
+            var response = _integration.ResponseFromAPIPost("", "v1/Registration/SetUserPasswordResetToken", new { userId = model.UserId, resetToken = model.ResetToken }, _integrationSettings.UsersDevApiUrl, true);
 
             if (response != null)
             {
@@ -101,7 +105,7 @@ namespace Feature.OHS.Web.Domain
             // encrypt before transmission
             model.Password = BCryptHelper.HashPassword(model.Password, BCryptHelper.GenerateSalt());
             model.ConfirmPassword = model.Password;
-            var response = _integration.ResponseFromAPIPost("", "v1/Registration/ResetPasswordAsync", model, "https://dev-feature-ohs-users-dot-medipark-hospital.appspot.com/", true);
+            var response = _integration.ResponseFromAPIPost("", "v1/Registration/ResetPasswordAsync", model, _integrationSettings.UsersDevApiUrl, true);
 
             if (response != null)
             {
